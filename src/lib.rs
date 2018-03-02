62722bdc49ed802b1d56640b99d1f5a5bbd01fc0
Mike Dilger <mike@optcomp.nz>
2018-03-03 11:57:55 +1300

    Documentation updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,109 @@
 // Copyright 2014-2018 Optimal Computing (NZ) Ltd.
 // Licensed under the MIT license.  See LICENSE for details.
 
+//! # float-cmp
+//!
 //! float-cmp defines traits for approximate comparison of floating point types which have fallen
 //! away from exact equality due to the limited precision available within floating point
 //! representations. Implementations of these traits are provided for `f32` and `f64` types.
 //!
-//! Two methods of comparison are provided. The first, `ApproxEqUlps` and `ApproxOrdUlps`, consider
-//! two comparands equal if the count of floating point representations between them is
-//! below a specified bound. This works well in most cases.
+//! Three methods of comparison are provided, but we recommend using the `ApproxEq` trait.
+//! We have left the other traits in the library for compatability and until we are certain that
+//! ApproxEq covers everything.
 //!
-//! The second method of comparison, `ApproxEqRatio`, considers two comparands equal if the ratio of
-//! the difference between them to the larger is below some specified bound. This handles many of
-//! the cases that the former type of comparison doesn't handle well.
+//! ApproxEq compares two floating point numbers by selecting one of two techniques, depending
+//! on the case. For two non-zero numbers of the same sign, the number of floating point
+//! representations that separate them are counted (an integer number), and must fall below
+//! a specifed bound that the caller supplies. This is an extremely fast operation. In all other
+//! cases, the difference between the two numbers (absolute) must be less than a given epsilon
+//! that the caller supplies.
 //!
-//! To help choose which comparison method to use, and to learn many suprising facts and oddities
-//! about floating point numbers, please refer to the following excellent website:
-//! https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
+//! ## The problem
 //!
-//! The trait `Ulps` is also defined.
-//!
-//! Floating point operations must round answers to the nearest representable number.  Multiple
-//! operations may result in an answer different from what you expect.  In the following example,
+//! Floating point operations must round answers to the nearest representable number. Multiple
+//! operations may result in an answer different from what you expect. In the following example,
 //! the assert will fail, even though the printed output says "0.45 == 0.45":
 //!
-//! ```should_fail
-//!   let a = 0.15_f32 + 0.15_f32 + 0.15_f32;
-//!   let b = 0.1_f32 + 0.1_f32 + 0.25_f32;
-//!   println!("{} == {}", a, b);
-//!   assert!(a==b)  // Fails, because they are not exactly equal
-//! ```
-//!
-//! This fails because the correct answer to most operations isn't exactly representable, and so your
-//! computer's processor chooses to represent the answer with the closest value it has available.
-//! This introduces error, and this error can accumulate as multiple operations are performed.
-//!
-//! With `ApproxEqUlps`, we can get the answer we intend:
-//!
-//! ```
+//! ```should_panic
 //!   # extern crate float_cmp;
-//!   # use float_cmp::ApproxEqUlps;
+//!   # use float_cmp::ApproxEq;
 //!   # fn main() {
 //!   let a = 0.15_f32 + 0.15_f32 + 0.15_f32;
 //!   let b = 0.1_f32 + 0.1_f32 + 0.25_f32;
 //!   println!("{} == {}", a, b);
-//!   assert!(a.approx_eq_ulps(&b,2)) // They are equal, within 2 ulps
+//!   assert!(a==b)  // Fails, because they are not exactly equal
 //!   # }
 //! ```
+//!
+//! This fails because the correct answer to most operations isn't exactly representable, and so
+//! your computer's processor chooses to represent the answer with the closest value it has
+//! available. This introduces error, and this error can accumulate as multiple operations are
+//! performed.
+//!
+//! ## The solution
+//!
+//! With `ApproxEq`, we can get the answer we intend:
+//!
+//! ```
+//!   # extern crate float_cmp;
+//!   # use float_cmp::ApproxEq;
+//!   # fn main() {
+//!   let a = 0.15_f32 + 0.15_f32 + 0.15_f32;
+//!   let b = 0.1_f32 + 0.1_f32 + 0.25_f32;
+//!   println!("{} == {}", a, b);
+//!   assert!(a.approx_eq(&b, 2, 2.0 * ::std::f32::EPSILON)) // They are equal, within 2 ulps
+//!   # }
+//! ```
+//!
+//! I recommend you use a small integer for the `ulps` parameter (2 to 10 or so), and a
+//! similar small multiple of the floating point's EPSILON constant (2.0 to 10.0 or so),
+//! but please send feedback if you think you have better advice.
+//!
+//! ## Some explanation
 //!
 //! We use the term ULP (units of least precision, or units in the last place) to mean the
 //! difference between two adjacent floating point representations (adjacent meaning that there is
 //! no floating point number between them). This term is borrowed from prior work (personally I
 //! would have chosen "quanta"). The size of an ULP (measured as a float) varies
-//! depending on the exponents of the floating point numbers in question, but this is quite useful,
-//! for it is the non-variation of a fixed epsilon (e.g. 0.0000001) which causes epsilon-based
-//! comparisons to so often fail with more extreme floating point values.
+//! depending on the exponents of the floating point numbers in question. That is a good thing,
+//! because as numbers fall away from equality due to the imprecise nature of their representation,
+//! they fall away in ULPs terms, not in absolute terms.  Pure epsilon-based comparisons are
+//! absolute and thus don't map well to the nature of the additive error issue. They work fine
+//! for many ranges of numbers, but not for others (consider comparing -0.0000000028
+//! to +0.00000097).
 //!
-//! Fixed epsilon systems of comparison tend to work well only on numbers within certain ranges.
-//! It may seem reasonable to expect numbers that differ by less than 0.000001 to be equal, but
-//! this does not always work well (consider comparing -0.0000000028 to +0.00000097).
+//! ## Implementing these traits
 //!
-//! You can implement `ApproxEqUlps` for your own complex types. The trait and type parameter
-//! notation can be a bit tricky, so here is an example:
+//! You can implement `ApproxEq` for your own complex types. The trait and type parameter
+//! notation can be a bit tricky, especially if your type is type parameterized around
+//! floating point types.  So here is an example (you'll probably not specify the Copy trait
+//! directly, but use some other NumTraits type floating point trait):
 //!
 //! ```
-//! use float_cmp::{ApproxEqUlps, Ulps};
+//! use float_cmp::{Ulps, ApproxEq};
 //!
 //! pub struct Vec2<F> {
 //!   pub x: F,
 //!   pub y: F,
 //! }
 //!
-//! impl<F: Ulps + ApproxEqUlps<Flt=F>> ApproxEqUlps for Vec2<F> {
+//! impl<F: Ulps + ApproxEq<Flt=F> + Copy> ApproxEq for Vec2<F> {
 //!   type Flt = F;
 //!
-//!   fn approx_eq_ulps(&self, other: &Self, ulps: <<F as ApproxEqUlps>::Flt as Ulps>::U) -> bool {
-//!     self.x.approx_eq_ulps(&other.x, ulps)
-//!       && self.y.approx_eq_ulps(&other.y, ulps)
+//!   fn approx_eq(&self, other: &Self, ulps: <<F as ApproxEq>::Flt as Ulps>::U,
+//!                epsilon: <F as ApproxEq>::Flt) -> bool
+//!   {
+//!     self.x.approx_eq(&other.x, ulps, epsilon)
+//!       && self.y.approx_eq(&other.y, ulps, epsilon)
 //!   }
 //! }
 //! ```
+//!
+//! ## Inspiration
+//!
+//! This crate was inspired by this Random ASCII blog post:
+//!
+//! [https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/](https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/)
 
 extern crate num_traits;
 
